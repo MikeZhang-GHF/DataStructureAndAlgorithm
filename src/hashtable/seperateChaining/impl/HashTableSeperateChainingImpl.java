43563558687b96fb11d886bf64166fca02e5f61b
mikego2019 <zhixiang.zhang2011@gmail.com>
2020-10-07 22:51:29 -0600

    Second commit

---
+++ src/hashtable/seperateChaining/impl/HashTableSeperateChainingImpl.java
@@ -0,0 +1,35 @@
+package hashtable.seperateChaining.impl;
+
+import hashtable.IHashTable;
+
+public class HashTableSeperateChainingImpl<K, V> implements IHashTable<K, V> {
+    @Override
+    public void put(Object key, Object val) {
+
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object delete(Object key) {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
