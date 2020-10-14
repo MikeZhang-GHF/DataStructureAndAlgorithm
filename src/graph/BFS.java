54d10a3357bcfaa05e74c418e43c6d8e5cee83a7
mikego2019 <zhixiang.zhang2011@gmail.com>
2020-10-13 20:52:14 -0600

    home commit

---
+++ src/graph/BFS.java
@@ -0,0 +1,96 @@
+package graph;
+
+import sun.awt.image.ImageWatched;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    public static void bfs(GraphNode graphNode) {
+        HashSet<GraphNode> visited = new HashSet<>();
+        Queue<GraphNode> queue = new LinkedList<>();
+
+        visited.add(graphNode);
+        queue.offer(graphNode);
+
+        while (!queue.isEmpty()) {
+            GraphNode node = queue.poll();
+            System.out.println(node.label);
+            for (GraphNode neighbor : node.neighbors) {
+                if (!visited.contains((neighbor))) {
+                    queue.offer(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void bfs2(GraphNode graphNode) {
+        HashSet<GraphNode> visited = new HashSet<>();
+        Queue<GraphNode> queue = new LinkedList<>();
+
+        visited.add(graphNode);
+        queue.offer(graphNode);
+
+        while (!visited.isEmpty()) {
+            // Get the node of every level
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                GraphNode node = queue.poll();
+                System.out.println(node.label);
+                for (GraphNode neigbor : node.neighbors) {
+                    if (!visited.contains(neigbor)) {
+                        queue.offer(neigbor);
+                        visited.add(neigbor);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void bfsMatrix(int[][] matrix) {
+
+    }
+
+    public static void main(String[] args) {
+        GraphNode a = new GraphNode(0);
+        GraphNode b = new GraphNode(1);
+        GraphNode c = new GraphNode(2);
+        GraphNode d = new GraphNode(3);
+        GraphNode e = new GraphNode(4);
+        GraphNode f = new GraphNode(5);
+
+        a.neighbors.add(b);
+        a.neighbors.add(d);
+        a.neighbors.add(f);
+
+        b.neighbors.add(a);
+        b.neighbors.add(e);
+        b.neighbors.add(c);
+
+        c.neighbors.add(b);
+        c.neighbors.add(e);
+
+        d.neighbors.add(a);
+        d.neighbors.add(e);
+
+        e.neighbors.add(d);
+        e.neighbors.add(b);
+        e.neighbors.add(c);
+
+        f.neighbors.add(a);
+
+        int[][] matrix = new int[][]{
+                {0, 1, 0, 1, 0, 1},
+                {1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 0, 1, 0},
+                {1, 0, 0, 0, 1, 0},
+                {0, 1, 1, 1, 0, 0},
+                {1, 0, 0, 0, 0, 0}
+        };
+
+        //bfs(a);
+        bfs2(a);
+    }
+}
